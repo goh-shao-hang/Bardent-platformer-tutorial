3004e5b511cb18c261f01f2129a853374e4cae17
GohTheDev <seanmaxgoh@gmail.com>
2022-10-15 19:19:16 +0800

    Uploaded project
    Progress: Ep 19

---
+++ Platformer/Assets/Scripts/Enemy/States/MoveState.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveState : State
+{
+    protected D_MoveState stateData;
+
+    protected bool isDetectingWall;
+    protected bool isDetectingLedge;
+    protected bool isPlayerInMinAggroRange;
+
+    public MoveState(Entity entity, FiniteStateMachine stateMachine, string animBoolName, D_MoveState stateData) : base(entity, stateMachine, animBoolName) //This means that the derived class will pass the parameters into the base class, allowing adding extra parameters for the derived class's constructor
+    {
+        this.stateData = stateData;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        
+        entity.SetVelocity(stateData.movementSpeed);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+
+    public override void DoChecks()
+    {
+        base.DoChecks();
+
+        isDetectingLedge = entity.CheckLedge();
+        isDetectingWall = entity.CheckWall();
+        isPlayerInMinAggroRange = entity.CheckPlayerInMinAggroRange();
+    }
+}
